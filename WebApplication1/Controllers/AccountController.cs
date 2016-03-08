23bef1486326a9a11d50cd43367873cc24f05369
anujarya300 <anujaryatitanic300@gmail.com>
2016-03-08 18:13:18 +0530

    Trigger Failed build 1

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -20,7 +20,7 @@ namespace WebApplication1.Controllers
 
         private string _msg;
         private string _msg1;
-        private string ;
+       
 
         public AccountController()
         {
@@ -30,6 +30,7 @@ namespace WebApplication1.Controllers
         {
             UserManager = userManager;
             SignInManager = signInManager;
+            fyufyu = "df";
         }
 
         public ApplicationSignInManager SignInManager
